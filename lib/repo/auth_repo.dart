d32ef05172b9f24f0defccb427bebf34445469de
jrmathew <mathewjohn93@gmail.com>
2021-10-01 11:00:23 +0530

    Settings and Auth repo

---
+++ lib/repo/auth_repo.dart
@@ -0,0 +1,78 @@
+import 'dart:convert';
+import 'package:cad_mobile/models/user_model.dart';
+import 'package:cad_mobile/network_module/api_path.dart';
+import 'package:cad_mobile/network_module/http_client.dart';
+import 'package:cad_mobile/route/route_generator.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AuthRepo {
+  Future<dynamic> logout(context) async {
+    final res = await HttpClient.instance
+        .fetchData(APIPathHelper.getValue(APIPath.logout));
+    // print(res.body);
+    if (res.statusCode == 200) {
+      print("Logout");
+      SharedPreferences localStorage = await SharedPreferences.getInstance();
+      localStorage.remove('user');
+      localStorage.remove('mobile');
+      localStorage.remove('token');
+      // localStorage.remove('flag');
+      // localStorage.remove('introDone');
+      // localStorage.remove('displayShow');
+      Navigator.of(context)
+          .pushNamedAndRemoveUntil(loginRoute, (Route<dynamic> route) => false);
+    }
+  }
+
+  Future<dynamic> signup(name, email, password, confirm, mobile) async {
+    var data = {
+      'name': name,
+      'email': email,
+      'password': password,
+      'password_confirmation': confirm,
+      'mobile': int.parse(mobile),
+      'buyer_name': name,
+    };
+
+    final res = await HttpClient.instance
+        .postData(APIPathHelper.getValue(APIPath.signup), data);
+
+    // print(res);
+    var body = json.decode(res.body);
+    if (res.statusCode == 200) {
+      SharedPreferences localStorage = await SharedPreferences.getInstance();
+      localStorage.setString('user', json.encode(body['user']));
+      localStorage.setString('mobile', body['user']['mobile'].toString());
+      localStorage.setString('token', json.encode(body['token']));
+      return true;
+    } else {
+      var body = json.decode(res.body);
+      return body;
+    }
+  }
+
+  Future<dynamic> login(var name, var pass) async {
+    var data = {
+      "mobile": name,
+      "password": pass,
+    };
+    final response = await HttpClient.instance
+        .postData(APIPathHelper.getValue(APIPath.login), data);
+    // print("Response - ${response['user']}");
+    // print(response.statusCode);
+    if (response.statusCode == 200) {
+      var body = json.decode(response.body);
+      User.fromJson(body['user']);
+      SharedPreferences localStorage = await SharedPreferences.getInstance();
+      localStorage.setString('mobile', body['user']['mobile'].toString());
+      localStorage.setString('token', json.encode(body['token']));
+      localStorage.setString('user', json.encode(body['user']));
+      // Navigator.of(context).pushReplacementNamed(authRoute);
+      return true;
+    } else {
+      var body = json.decode(response.body);
+      return body;
+    }
+  }
+}
---
+++ lib/repo/settings_repo.dart
@@ -0,0 +1,73 @@
+import 'dart:convert';
+import 'package:cad_mobile/network_module/api_path.dart';
+import 'package:cad_mobile/network_module/http_client.dart';
+import 'package:cad_mobile/utils/customDialog.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SettingsRepo {
+  Future<dynamic> handleContactInfo(
+      BuildContext context, name, bname, phone, email, tick) async {
+    CustomDialog cD = CustomDialog();
+    SharedPreferences localStorage = await SharedPreferences.getInstance();
+    var user = jsonDecode(localStorage.getString('user'));
+    // String gen = types[selectedType];
+    var data = {
+      'user_id': user['id'],
+      'name': name,
+      'buyer_name': bname,
+      'email': email,
+      'mobile': phone,
+      'showSellerDetails': tick
+    };
+    final res = await HttpClient.instance
+        .fetchParamData(APIPathHelper.getValue(APIPath.add_ContactInfo), data);
+
+    print(res.body);
+    if (res.statusCode == 200) {
+      cD.successFlushbar(
+        context,
+        "Updation Complete ",
+      );
+    } else {
+      cD.failedFlushbar(
+        context,
+        "Check all input fields",
+      );
+    }
+  }
+
+  change(BuildContext context, old, newP, confirm) async {
+    CustomDialog cD = CustomDialog();
+    SharedPreferences localStorage = await SharedPreferences.getInstance();
+    var user = jsonDecode(localStorage.getString('user'));
+    var data = {
+      'user_id': user['id'],
+      'oldPassword': old,
+      'password': newP,
+      'password_confirmation': confirm,
+    };
+    final res = await HttpClient.instance
+        .fetchParamData(APIPathHelper.getValue(APIPath.changePassword), data);
+    if (res.body == '0') {
+      cD.successFlushbar(context, "Password Successfully Changed");
+    } else {
+      cD.failedFlushbar(context, "Password Change Unsuccessful");
+    }
+  }
+
+  loadUserData() async {
+    SharedPreferences localStorage = await SharedPreferences.getInstance();
+    var user = jsonDecode(localStorage.getString('user'));
+    var data = {
+      'id': user['id'],
+    };
+    final res = await HttpClient.instance
+        .fetchParamData(APIPathHelper.getValue(APIPath.get_User), data);
+    // print(res.body["name"]);
+    if (res.statusCode == 200 && res.body != "") {
+      var responseJson = json.decode(res.body);
+      return responseJson;
+    }
+  }
+}
